37fd24ad9ae702140f50e3b30e2bb77ba01caa0e
DESKTOP-NV8KUKE\yusuk <yusukesasaki0601@gmail.com>
2025-11-27 23:13:15 +0900

    front 修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,27 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
-import rollupNodePolyFill from "rollup-plugin-node-polyfills";
+import nodePolyfills from "vite-plugin-node-polyfills";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    nodePolyfills({
+      protocolImports: true,
+    }),
+  ],
   resolve: {
     alias: {
       crypto: "crypto-browserify",
       stream: "stream-browserify",
-      assert: "assert",
       util: "util",
+      assert: "assert",
     },
   },
   optimizeDeps: {
     esbuildOptions: {
-      plugins: [NodeModulesPolyfillPlugin()],
       define: {
         global: "globalThis",
       },
-    },
-  },
-  build: {
-    rollupOptions: {
-      plugins: [rollupNodePolyFill()],
-    },
-    commonjsOptions: {
-      transformMixedEsModules: true,
     },
   },
 });
